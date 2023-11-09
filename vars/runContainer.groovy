013b67e3a2d2a2c1a67fa3ec68ecc14795e4c08c
krisforu <krisforu@gmail.com>
2023-11-09 11:49:01 +0530

    Update runContainer.groovy

--- vars/runContainer.groovy
+++ vars/runContainer.groovy
@@ -1,3 +1,3 @@
 def call(){
-    sh "docker run -d --name youtube1 -p 8211:3000 krisoru/youtube:latest"
+    sh "docker run -d --name youtube1 -p 8211:3000 ${dockerHubUsername}/${imageName}:latest"
 }
