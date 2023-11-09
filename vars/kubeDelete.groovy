2e99623567e8de4f1028e92ed6f1fd15a8d62f97
krisforu <krisforu@gmail.com>
2023-11-09 11:47:17 +0530

    Update kubeDelete.groovy

--- vars/kubeDelete.groovy
+++ vars/kubeDelete.groovy
@@ -1,5 +1,5 @@
 def call() {
     withKubeConfig(caCertificate: '', clusterName: '', contextName: ${KUBERNETES_SERVER_URL}, credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: ${CONTEXT_NAME} ) {
-        sh "kubectl delete -f deployment.yml"
+        sh "kubectl delete -f Kubernetes/Kubernetes/deployment.yml"
     }
 }
