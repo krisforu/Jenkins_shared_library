27605221d0407a8d2d51bf852e5559a9d77de7a0
krisforu <krisforu@gmail.com>
2023-11-14 17:29:54 +0530

    Update kubeDeploy.groovy

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -1,4 +1,4 @@
-def call() {
+def call(String KubernetesContextName, String KubernetesServerUrl) {
     withKubeConfig(caCertificate: '', clusterName: '', contextName: ${KubernetesContextName}, credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: ${KubernetesServerUrl} ) {
         sh "kubectl apply -f  Kubernetes/Kubernetes/deployment.yml"
     }
